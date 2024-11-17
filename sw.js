7a019afb816c31d53ab4a453567a3f983f2f7125
Larry-leee06 <3363397004@qq.com>
2024-11-18 00:02:00 +0800

    Site updated: 2024-11-18 00:02:00

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1731859190614";
+const VERSION = "1731859314777";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
